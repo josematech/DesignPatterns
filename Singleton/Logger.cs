ce6e94630df0ba99461d32e55e5754680f3d09e0
Jose-Manuel Fernandez <jose-manuel_fernandez@keysight.com>
2022-06-27 21:47:05 +0200

    Add project files.

---
+++ Singleton/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+            }
+        }
+
+        protected Logger()
+        {
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Instance.Log("Hello...");
+            Logger.Instance.Log("...I'm always using the same instance...");
+            Logger.Instance.Log("...of the logger class");
+            Console.ReadKey();
+        }
+    }
+}
